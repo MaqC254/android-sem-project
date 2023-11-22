139d70ac4da79628f754efc129922278de7b6115
Maxwell Gacuca <maxwellm751@gmail.com>
2023-11-22 10:58:28 +0300

    CourseRegistration design remains

---
+++ ExaminationProcessingSystem/app/src/main/java/com/example/examinationprocessingsystem/CourseRegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.example.examinationprocessingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CourseRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_registration);
+    }
+}
---
+++ ExaminationProcessingSystem/app/src/main/java/com/example/examinationprocessingsystem/StaffSignUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.examinationprocessingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StaffSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_sign_up);
+    }
+}
